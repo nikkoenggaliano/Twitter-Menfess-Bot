b6ca19b266d187a592de4bdc7bd6bb4dbbb3ebcb
nikko <nikkoenggaliano@gmail.com>
2020-06-07 07:23:26 +0700

    Submit template for configure API

---
+++ config.example.py
@@ -0,0 +1,24 @@
+#Example config
+
+config = {
+	
+	'delay' : 0,
+	'keywords': ["!rp", "!base"],
+	'consumer_key' : "",
+	'consumer_secret' : "",
+	'access_token' : "",
+	'access_token_secret' : ""
+
+}
+
+
+# config = {
+	
+# 	'delay' : 0,
+# 	'keywords': ["!rp", "!base"],
+# 	'consumer_key' : "",
+# 	'consumer_secret' : "",
+# 	'access_token' : "",
+# 	'access_token_secret' : ""
+
+# }
